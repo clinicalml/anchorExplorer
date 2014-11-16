ed98266546b069acfe359704842b6df2f0e5b3d9
yhalpern <yhalpern@gmail.com>
2014-11-16 15:07:47 -0500

    now reads age and sex from mimic data

--- get_mimic_data.py
+++ get_mimic_data.py
@@ -1,25 +1,11 @@
 import sys
+import string
 import re
 import csv
 import os
 
-_illegal_unichrs = [(0x00, 0x08), (0x0B, 0x0C), (0x0E, 0x1F), 
-                        (0x7F, 0x84), (0x86, 0x9F), 
-                        (0xFDD0, 0xFDDF), (0xFFFE, 0xFFFF)] 
-if sys.maxunicode >= 0x10000:  # not narrow build 
-        _illegal_unichrs.extend([(0x1FFFE, 0x1FFFF), (0x2FFFE, 0x2FFFF), 
-                                 (0x3FFFE, 0x3FFFF), (0x4FFFE, 0x4FFFF), 
-                                 (0x5FFFE, 0x5FFFF), (0x6FFFE, 0x6FFFF), 
-                                 (0x7FFFE, 0x7FFFF), (0x8FFFE, 0x8FFFF), 
-                                 (0x9FFFE, 0x9FFFF), (0xAFFFE, 0xAFFFF), 
-                                 (0xBFFFE, 0xBFFFF), (0xCFFFE, 0xCFFFF), 
-                                 (0xDFFFE, 0xDFFFF), (0xEFFFE, 0xEFFFF), 
-                                 (0xFFFFE, 0xFFFFF), (0x10FFFE, 0x10FFFF)]) 
 
-_illegal_ranges = ["%s-%s" % (unichr(low), unichr(high)) 
-                   for (low, high) in _illegal_unichrs] 
-#_illegal_xml_chars_RE = re.compile(u'[%s]' % u''.join(_illegal_ranges)+)
-_illegal_xml_chars_RE = re.compile('&')
+
 
 MAX_ENTRIES = 2
 def collectInfo(datadir, patient, structure):
@@ -36,13 +22,13 @@ def collectInfo(datadir, patient, structure):
             header = reader.next()
             index = header.index(field[0])
             for n,l in enumerate(reader):
-                data = ''
-                data += l[index].strip() +'\n'
+                data = l[index].strip() +'\n'
                 if len(field[1]):
                     print "\t\t<"+field[1]+">" +re.sub("\&", "", data) +"</"+field[1]+">"
-                    #print "\t\t<"+field[1]+">" +data +"</"+field[1]+">"
+                    
                 else:
-                    print re.sub("\&", "", data)
+                    if n == 0:
+                        print re.sub("\&", "", data)
         except:
             pass
 
@@ -76,6 +62,5 @@ if __name__ == "__main__":
             fieldnames = l.strip().split(':')
             structure[fname] = fieldnames
 
-    #collectInfo('00', '00077', structure)
     for n,pat in enumerate(os.listdir(src)):
         collectInfo(src, pat, structure)
