b43570fb45cf2ce4d27678d67bc2d5139e106d32
yhalpern <yhalpern@gmail.com>
2014-11-16 15:27:56 -0500

    script to download icd9 structure

---
+++ examples/ICD9/load_ICD9_structure.py
@@ -0,0 +1,43 @@
+import sys
+import os
+import subprocess
+import string
+
+printable = set(string.printable)
+def sanitize(txt):
+    txt = ''.join(filter(lambda c: c in printable, txt)) 
+    return txt
+
+def traverse(t, outfile):
+    print>>outfile, sanitize(t.code+'\t'+t.description)
+    for c in t.children:
+        traverse(c, outfile)
+
+def getEdges(t, outfile):
+    for c in t.children:
+        print >>outfile, sanitize(t.code+'\t'+c.code)
+        getEdges(c, outfile)
+
+
+print 'cloning github repository sirrice/icd9.git'
+#subprocess.call('git clone https://github.com/sirrice/icd9.git')
+
+sys.path.append('icd9')
+from icd9 import ICD9
+
+tree = ICD9('icd9/codes.json')
+toplevelnodes = tree.children
+
+print 'creating name file'
+outfile = file('code.names', 'w')
+traverse(tree, outfile)
+outfile.close()
+
+print 'creating edges file'
+outfile = file('code.edges', 'w')
+getEdges(tree, outfile)
+outfile.close()
+
+print 'cleaning up'
+os.chdir('..')
+#subprocess.call('rm -rf icd9', shell=1)
