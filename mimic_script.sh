8175ec27bb51abbdfeb38b6b7c8b8eb3abf1aa0b
yhalpern <yhalpern@gmail.com>
2014-11-17 11:39:54 -0500

    usage info

--- mimic_script.sh
+++ mimic_script.sh
@@ -1,4 +1,9 @@
 
+if [ -z "$1" ]
+then
+  echo "usage: ./mimic_script.sh path_to_mimic"
+  exit
+fi
 MIMIC_PATH=$1
 echo "Reading mimic data from $MIMIC_PATH"
 python get_mimic_data.py $MIMIC_PATH/00 examples/mimic_fields.txt> patients.xml
